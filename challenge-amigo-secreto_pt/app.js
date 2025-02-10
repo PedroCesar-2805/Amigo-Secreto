b01f392c6c185f1e2c2c3b61fd1f29b92809ea57
PedroCesar-2805 <cesarpedro159@gmail.com>
2025-02-09 23:27:44 -0300

    Anexar a função adicionarAmigo

--- challenge-amigo-secreto_pt/app.js
+++ challenge-amigo-secreto_pt/app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+function adicionarAmigo(){
+    let validarNome = document.querySelector('input').value;
+    if(validarNome == ''){
+        alert("Por favor, insira um nome.")
+    }else{
+        amigos.push(validarNome)
+        console.log(amigos)
+        limparCampo()
+        
+    }    
+};
+ function limparCampo() {
+    validarNome = document.querySelector('input');
+    validarNome.value = '';
+ }
+ 
