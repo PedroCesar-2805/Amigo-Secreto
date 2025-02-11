e57d6ebd7a47f1a9056f31a78e70a6a6fc1badd4
PedroCesar-2805 <cesarpedro159@gmail.com>
2025-02-11 00:49:12 -0300

    Finalizando o projeto

--- challenge-amigo-secreto_pt/app.js
+++ challenge-amigo-secreto_pt/app.js
@@ -7,12 +7,37 @@ function adicionarAmigo(){
     }else{
         amigos.push(validarNome)
         console.log(amigos)
+        atualizarLista()
         limparCampo()
-        
+       
     }    
 };
  function limparCampo() {
     validarNome = document.querySelector('input');
     validarNome.value = '';
  }
- 
+ function atualizarLista() {
+    let listaDeAmigos = document.getElementById("listaAmigos");
+    listaDeAmigos.innerHTML = ""; 
+
+    for (const amigo of amigos) {
+        let li = document.createElement("li"); 
+        li.textContent = amigo; 
+        listaDeAmigos.appendChild(li); 
+    }
+}
+function sortearAmigo(){
+    let resultado = document.getElementById("resultado"); 
+    resultado.innerHTML= "";
+    if(amigos.length>=2){
+        let indiceAleatorio= Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        let li = document.createElement("li"); 
+        li.textContent = `O seu amigo secreto é ${amigoSorteado}. `; 
+        resultado.appendChild(li); 
+    }else if(amigos.length==1){
+        alert("Você precisa adcionar pelo mais um amigo para ter o sorteio.")
+    }else{
+        alert("Adicione amigos.")
+    }
+}
